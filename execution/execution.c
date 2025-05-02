15cb183cfe7da8021a2551df1a5d794e6132368f
supalukp <supaluk.pun@gmail.com>
2025-05-02 15:05:52 +0200

    feat: main_execution

--- execution/execution.c
+++ execution/execution.c
@@ -6,35 +6,206 @@
 /*   By: spunyapr <spunyapr@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 09:10:05 by spunyapr          #+#    #+#             */
-/*   Updated: 2025/04/30 16:03:46 by spunyapr         ###   ########.fr       */
+/*   Updated: 2025/05/02 15:04:16 by spunyapr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "execution.h"
+#include "../inc/headings.h"
 
-int main_execution(t_token *tree)
+int	main_execution(t_token *tree, char **env)
 {
-    int exit_status;
-    
-    // Build a tree start from root
-    if (!tree)
-        return (0);
-    if (tree->type == PIPE)
-        exit_status = pipe_process(tree); // TODO 
-    else if (tree->type == CMD)
-        exit_status = simple_command_process(tree); // TODO
-    else
-        return (handle_unexpected_type(tree)); // TODO
+	int	exit_status;
+
+	exit_status = 0;
+	// Build a tree start from root
+	if (!tree)
+		return (0);
+	if (tree->type == PIPE)
+		exit_status = pipe_process(tree, env); // TODO
+	else if (tree->type == CMD)
+    {
+        is_buildin(tree->name);
+
+		exit_status = simple_command_process(tree, env); // TODO
+    }
+	// else
+	//     return (handle_unexpected_type(tree)); // TODO
+	return (exit_status);
 }
 
-int pipe_process(t_token *token)
+char	*find_executable(char **all_path, char *command)
 {
-    if (!token)
-        return (0);
-    
+	int		j;
+	char	*add_slash;
+	char	*path_command;
+
+	j = 0;
+	while (all_path[j] != NULL)
+	{
+		add_slash = ft_strjoin(all_path[j], "/");
+		if (!add_slash)
+			return (free_matrix(all_path), NULL);
+		path_command = ft_strjoin(add_slash, command);
+		if (!path_command)
+			return (free_matrix(all_path), NULL);
+		free(add_slash);
+		if (access(path_command, F_OK | X_OK) == 0)
+		{
+			free_matrix(all_path);
+			return (ft_strdup(path_command));
+		}
+		free(path_command);
+		j++;
+	}
+	return (free_matrix(all_path), NULL);
 }
 
+char	*get_path(char *command, char **envp)
+{
+	int		i;
+	char	**all_path;
 
+	i = 0;
+	if (!command || !envp)
+		return (NULL);
+	while (envp[i] != NULL)
+	{
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+		{
+			all_path = ft_split(ft_strchr(envp[i], '=') + 1, ':');
+			if (!all_path)
+				return (NULL);
+			return (find_executable(all_path, command));
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+void	free_matrix(char **matrix)
+{
+	int	i;
+
+	i = 0;
+	if (!matrix)
+		return ;
+	while (matrix[i])
+	{
+		free(matrix[i]);
+		matrix[i] = NULL;
+		i++;
+	}
+	free(matrix);
+	matrix = NULL;
+}
+
+// int simple_command_process(t_token *tree, char **env)
+// {
+//     int exit_status;
+//     char **args;
+//     char *paths;
+
+//     args = ft_split(tree->name, ' ');
+//     // if (!args)
+//     //     exit_close_fd();
+//     paths = get_path(args[0], env);
+//     // if (!paths)
+//     //     exit_close_fd();
+
+//     // Check if there is REDIRECT
+//     // if (tree->   ->type == INFILE || )
+//     //     set_up_redirect();
+
+//     execve(paths, args, NULL);
+//     free_matrix(args);
+//     //exit_error();
+// }
+
+int	simple_command_process(t_token *tree, char **env)
+{
+	pid_t	pid;
+	int		exit_status;
+	char	**args;
+	char	*paths;
+
+	args = ft_split(tree->name, ' ');
+	if (!args)
+		return (1);
+	paths = get_path(args[0], env);
+	if (!paths)
+	{
+		free_matrix(args);
+		return (127);
+	}
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork failed");
+		free_matrix(args);
+		free(paths);
+		return (1);
+	}
+	if (pid == 0)
+	{
+		execve(paths, args, env);
+		perror("execve failed");
+		free_matrix(args);
+		free(paths);
+		exit(1);
+	}
+	else
+	{
+		waitpid(pid, &exit_status, 0);
+		free_matrix(args);
+		free(paths);
+		if (WIFEXITED(exit_status))
+			return (WEXITSTATUS(exit_status));
+		else
+			return (1);
+	}
+}
+
+int	pipe_process(t_token *tree, char **env)
+{
+	int		pipefd[2];
+	pid_t	pid1;
+	pid_t	pid2;
+	int		exit_status;
+
+	exit_status = 0;
+	if (pipe(pipefd) == -1)
+		return (perror("pipe failed"), EXIT_FAILURE);
+	pid1 = fork();
+	if (pid1 == -1)
+		return (perror("fork failed"), EXIT_FAILURE);
+	if (pid1 == 0)
+	{
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT_FILENO);
+		close(pipefd[1]);
+		exit(main_execution(tree->left, env));
+	}
+	pid2 = fork();
+	if (pid2 == -1)
+		return (perror("fork"), EXIT_FAILURE);
+	if (pid2 == 0)
+	{
+		close(pipefd[1]);
+		dup2(pipefd[0], STDIN_FILENO);
+		close(pipefd[0]);
+		exit(main_execution(tree->right, env));
+	}
+	close(pipefd[0]);
+	close(pipefd[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, &exit_status, 0);
+	if (WIFEXITED(exit_status))
+		return (WEXITSTATUS(exit_status));
+	return (EXIT_FAILURE);
+}
+
+	// else if (WIFSIGNALED(exit_status))
+		//     return (128 + WTERMSIG(exit_status));
 // void execute_buidin(t_branch *branch);
 // void each_branch_execute(t_branch *branch);
 // void execute_external(t_branch *branch);
@@ -72,11 +243,11 @@ int pipe_process(t_token *token)
 // {
 //     char *cmd;
 //     int exit_status;
-    
+
 //     exit_status = 1;
 //     cmd = branch->first_token;
 //     if (!ft_strcmp(cmd, "echo"))
-//         exit_status = ft_echo(); 
+//         exit_status = ft_echo();
 //     else if (!ft_strcmp(cmd, "cd"))
 //         exit_status = ft_cd();
 //     else if (!ft_strcmp(cmd, "pwd"))
@@ -99,8 +270,8 @@ int pipe_process(t_token *token)
 // void execute_external(t_branch *branch)
 // {
 //     /*
-//         // Fork 
-        
+//         // Fork
+
 //         // In child process
 //         if (pid == 0)
 //         {
@@ -114,4 +285,3 @@ int pipe_process(t_token *token)
 //         }
 //     */
 // }
-
--- execution/my_init.c
+++ execution/my_init.c
@@ -6,61 +6,76 @@
 /*   By: spunyapr <spunyapr@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 16:26:23 by spunyapr          #+#    #+#             */
-/*   Updated: 2025/04/29 17:44:30 by spunyapr         ###   ########.fr       */
+/*   Updated: 2025/05/02 15:01:57 by spunyapr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "headings.h"
+#include "../inc/headings.h"
 
-void pipe_simple_input(t_token *tree)
+void print_ast(t_token *node) 
 {
-    // cmd = ls -al | wc -l  
-    tree->type = PIPE;
-    tree->name = "|";
-    
-    // Left    
-    tree->left = malloc(sizeof(t_token));
-    tree->left->type = CMD;
-    tree->left->name = "ls -al";
-    tree->left->left = NULL;
-    tree->left->right = NULL;
-
-    // Right
-    tree->right = malloc(sizeof(t_token));
-    tree->right->type = CMD;
-    tree->right->name = "wc -l";
-    tree->right->left = NULL;
-    tree->right->right = NULL;
+    if (!node)
+        return;
+    if (node->type == PIPE)
+        printf("PIPE: %s\n", node->name);
+    else if (node->type == CMD)
+        printf("CMD: %s\n", node->name);
+    print_ast(node->left);
+    print_ast(node->right);
 }
 
-void pipe_double_input(t_token *tree)
+void pipe_simple_input(t_token **tree)
+{
+    // cmd = ls -al | wc -l  
+    (*tree) = malloc(sizeof(t_token));
+    (*tree)->type = PIPE;
+    (*tree)->name = ft_strdup("|");
+    
+    // Left    
+    (*tree)->left = malloc(sizeof(t_token));
+    (*tree)->left->type = CMD;
+    (*tree)->left->name = ft_strdup("ls -al");
+    (*tree)->left->left = NULL;
+    (*tree)->left->right = NULL;
+
+    // Right
+    (*tree)->right = malloc(sizeof(t_token));
+    (*tree)->right->type = CMD;
+    (*tree)->right->name = ft_strdup("rev");
+    (*tree)->right->left = NULL;
+    (*tree)->right->right = NULL;
+}
+
+void pipe_double_input(t_token **tree)
 {
     // cmd = ls -al | wc -l | cat -e
-    tree->type = PIPE;
-    tree->name = "|";
+    (*tree) = malloc(sizeof(t_token));
+    (*tree)->type = PIPE;
+    (*tree)->name = ft_strdup("|");
     
     // TREE-RIGHT
-    tree->right = malloc(sizeof(t_token));
-    tree->right->type = CMD;
-    tree->right->name = "cat -e";
-    tree->right->left = NULL;
-    tree->right->right = NULL;
+    (*tree)->right = malloc(sizeof(t_token));
+    (*tree)->right->type = CMD;
+    (*tree)->right->name = "rev";
+    (*tree)->right->left = NULL;
+    (*tree)->right->right = NULL;
     
     // TREE-LEFT
-    tree->left = malloc(sizeof(t_token));
-    tree->left->type = PIPE;
-    tree->left->name = "|";
+    (*tree)->left = malloc(sizeof(t_token));
+    (*tree)->left->type = PIPE;
+    (*tree)->left->name = ft_strdup("|");
     
-    // TREE-LEFT Left    
-    tree->left->left->type = CMD;
-    tree->left->left->name = "ls -al";
-    tree->left->left->left = NULL;
-    tree->left->left->right = NULL;
+    // TREE-LEFT Left   
+    (*tree)->left->left = malloc(sizeof(t_token)); 
+    (*tree)->left->left->type = CMD;
+    (*tree)->left->left->name = ft_strdup("ls -al");
+    (*tree)->left->left->left = NULL;
+    (*tree)->left->left->right = NULL;
 
     // TREE-LEFT Right
-    tree->left->right = malloc(sizeof(t_token));
-    tree->left->right->type = CMD;
-    tree->left->right->name = "wc -l";
-    tree->left->right->left = NULL;
-    tree->left->right->right = NULL;
+    (*tree)->left->right = malloc(sizeof(t_token));
+    (*tree)->left->right->type = CMD;
+    (*tree)->left->right->name = ft_strdup("wc -l");
+    (*tree)->left->right->left = NULL;
+    (*tree)->left->right->right = NULL;
 }
