7597acfbdaa2a7809550a43e10cbd7e5aa0d2e6d
supalukp <supaluk.pun@gmail.com>
2025-05-24 09:33:28 +0200

    feat: error message

--- execution/error_msg.c
+++ execution/error_msg.c
@@ -6,14 +6,17 @@
 /*   By: spunyapr <spunyapr@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 12:51:48 by spunyapr          #+#    #+#             */
-/*   Updated: 2025/05/22 14:19:15 by spunyapr         ###   ########.fr       */
+/*   Updated: 2025/05/24 09:21:21 by spunyapr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/headings.h"
 
-void    print_error_msg(char *error_msg, int errno)
+void    print_file_err(char *filename, int errnum)
 {
-    printf("%s: %s", error_msg, strerror(errno));
+    write(STDERR_FILENO, "minishell: ", 12);
+    write(STDERR_FILENO, filename, strlen(filename));
+    write(STDERR_FILENO, ": ", 2);
+    write(STDERR_FILENO, strerror(errnum), strlen(strerror(errnum)));
+    write(STDERR_FILENO, "\n", 1);
 }
-
